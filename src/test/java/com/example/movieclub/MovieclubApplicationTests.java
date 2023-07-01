78fa92ccfee3b02344d40756e7dbab07e9eb410f
wwojtas <wojciech.wus.programista@gmail.com>
2023-07-01 13:56:35 +0200

    First step - create front-end

---
+++ src/test/java/com/example/movieclub/MovieclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieclubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
